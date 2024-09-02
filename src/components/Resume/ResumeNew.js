e6dd180606ad7969b6f508adaec281c9b07d17d2
ShahilRaza <shahil4278@gmail.com>
2024-09-02 09:55:17 -0700

    frist

--- src/components/Resume/ResumeNew.js
+++ src/components/Resume/ResumeNew.js
@@ -2,7 +2,7 @@ import React, { useState, useEffect } from "react";
 import { Container, Row } from "react-bootstrap";
 import Button from "react-bootstrap/Button";
 import Particle from "../Particle";
-import pdf from "../../Assets/../Assets/Soumyajit_Behera-BIT_MESRA.pdf";
+import pdf from "../../Assets/../Assets/ShariqKhanCv.pdf";
 import { AiOutlineDownload } from "react-icons/ai";
 import { Document, Page, pdfjs } from "react-pdf";
 import "react-pdf/dist/esm/Page/AnnotationLayer.css";
@@ -54,3 +54,11 @@ function ResumeNew() {
 }
 
 export default ResumeNew;
+
+
+
+
+
+
+
+
